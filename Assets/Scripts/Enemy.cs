91f226f777b81ae61c10e4f313612b8a4383ab79
Eric_vom_Bovert <ericvombovert.germany@gmail.com>
2019-10-26 14:56:15 +0200

    Weitere Bugs gefixt
    Es funktioniert immernoch

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,9 +15,11 @@ public class Enemy : MonoBehaviour
     public int health=2;
     private void Start()
     {
+	/*
         PlayerMobility playerMobility = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMobility>();
         player = playerMobility;
-        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		*/
     }
  
 
