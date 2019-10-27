79d4cc4637e1085f25c8a1400bb7f6ab92eea828
Eric_vom_Bovert <ericvombovert.germany@gmail.com>
2019-10-27 19:07:05 +0100

    ES IST JETZT EIN RICHTIGES SPIEL!!
    ich bin zufrieden :3

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public GameObject[] Hearts;
+	public GameObject player;
+	public Sprite HealthSprite;
+	public Sprite DeadSprite;
+	private SpriteRenderer sr;
+    public int MaxHealth;
+	public int CurrentHealth;
+
+	void Start(){
+	MaxHealth=5;
+	for(int i=MaxHealth; i<6; i++){
+	Hearts[i].GetComponent<Renderer>().enabled = false;
+	}
+	}
+
+void Update(){
+CurrentHealth=player.GetComponent<PlayerMobility>().Hp;
+if(CurrentHealth<0){
+	CurrentHealth=0;
+}
+if(CurrentHealth>MaxHealth){
+	CurrentHealth=MaxHealth;
+}
+for(int i=0; i<CurrentHealth; i++){
+life(i);
+
+}
+for(int i=CurrentHealth; i<MaxHealth; i++){
+death(i);
+
+}
+
+
+ 
+
+}
+
+
+public void life(int i) {
+    sr=Hearts[i].GetComponent<SpriteRenderer>();
+	sr.sprite = HealthSprite;
+}
+    
+public void death(int i) {
+    sr=Hearts[i].GetComponent<SpriteRenderer>();
+	sr.sprite = DeadSprite;
+}
+
+}
