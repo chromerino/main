c7f66e13d8dfe9017bb82790022f8b8613918a16
Doktor Chrome <nils.palenio@googlemail.com>
2019-10-29 15:09:55 +0100

    Kamera changes und Itemspawner

---
+++ Assets/Scripts/itemspawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemspawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] spawnSpots;
+    public GameObject[] items;
+    private float timeBtwSpawns;
+        public float startTimeBtwSpawns;
+    private void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+    void Update()
+    {
+        if(timeBtwSpawns <= 0)
+        {
+            int randPos = Random.Range(0, spawnSpots.Length-1);
+            int randPos2= Random.Range(0, items.Length-1) ;
+            Instantiate(items[randPos2], spawnSpots[randPos].position, spawnSpots[randPos].rotation);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+
+}
